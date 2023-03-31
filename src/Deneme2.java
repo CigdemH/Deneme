39e47d6129cfd4d5ac551654a153504ab9531a7d
CigdemH <hkz34ing@gmail.com>
2023-03-31 12:47:24 +0300

    ilk deneme hayirli olsun

---
+++ src/Deneme2.java
@@ -0,0 +1,5 @@
+public class Deneme2 {
+    public static void main(String[] args) {
+        System.out.println("deneme2 hayirli olsun");
+
+    }}
---
+++ src/Deneme_1/deneme3.java
@@ -0,0 +1,8 @@
+package Deneme_1;
+
+public class deneme3 {
+    public static void main(String[] args) {
+        System.out.println("deneme 3 hayirli olsun");
+
+    }
+}
