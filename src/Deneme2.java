0b0a2b3a165253d5dbd9c7f49113a296e8a044b2
CigdemH <hkz34ing@gmail.com>
2023-03-31 12:58:38 +0300

    5. deneme

--- src/Deneme2.java
+++ src/Deneme2.java
@@ -2,5 +2,6 @@ public class Deneme2 {
     public static void main(String[] args) {
         System.out.println("deneme2 hayirli olsun");
         System.out.println("4 deneme hayirli olsun");
+        so
 
     }}
--- src/Deneme_1/deneme3.java
+++ src/Deneme_1/deneme3.java
@@ -3,6 +3,7 @@ package Deneme_1;
 public class deneme3 {
     public static void main(String[] args) {
         System.out.println("deneme 3 hayirli olsun");
+        System.out.println("5.Deneme hayirli olsun");
 
     }
 }
